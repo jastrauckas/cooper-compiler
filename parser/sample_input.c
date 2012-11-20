90ed96537c015010abe445b3b94f43efb889d86c
Julia <julia@batcomputer.(none)>
2012-11-20 17:42:32 -0500

    with test in/out

---
+++ parser/sample_input.c
@@ -0,0 +1,25 @@
+struct foo {
+	int a;
+} f;
+struct foo *fp;
+struct bar *bp;
+
+int i;
+long double dd;
+fn() {
+	long l;
+}
+int l;
+char l;
+
+struct s {
+	int i[50];
+	double j;
+	char *k;
+} s;
+
+int *intarray[10];
+
+
+
+
